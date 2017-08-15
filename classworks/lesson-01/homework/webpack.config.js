4efe9e4fda6e38918131eb3a2b7c8781858fff65
maksim <gtl.vorobyov@gmail.com>
2017-08-15 19:05:41 +0300

    1

--- classworks/lesson-01/homework/webpack.config.js
+++ classworks/lesson-01/homework/webpack.config.js
@@ -1,45 +1,43 @@
 const webpack = require('webpack');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: './bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['react', 'env']
-					}
-				}
-			},
-			{
-				test: /\.css/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf)$/i,
-				loader: "file-loader?name=fonts/[name]-[hash].[ext]"
-			},
-			{
-				test: /\.(jpe?g|png|gif|svg)$/i,
-				loader: "file-loader?name=images/[name]-[hash].[ext]"
-			}
-		]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	devServer: {
-		open: true,
-		port: 3000,
-		inline: true,
-		hot: true
-	}
-
+  entry: './src/index.js',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env'],
+          },
+        },
+      },
+      {
+        test: /\.css/,
+        exclude: /(node_modules)/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/i,
+        loader: 'file-loader?name=fonts/[name]-[hash].[ext]',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader?name=images/[name]-[hash].[ext]',
+      },
+    ],
+  },
+  devtool: 'cheap-module-eval-source-map',
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    open: true,
+    port: 3000,
+    inline: true,
+    hot: true,
+  },
 };
