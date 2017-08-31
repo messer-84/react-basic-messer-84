0aca579dd3a3209e7350eb3a8194c7372611d972
maksim <gtl.vorobyov@gmail.com>
2017-08-31 13:27:07 +0300

    app-4

--- classworks/lesson-06/classwork/les6/src/reducers/index.js
+++ classworks/lesson-06/classwork/les6/src/reducers/index.js
@@ -1,12 +1,9 @@
 import { combineReducers } from 'redux';
 
 import counterReducer from './counter-reducer';
-// import historyReducer from './history-reducer';
 
 const rootReducer = combineReducers({
-	counterReducer: counterReducer,
-	// historyReducer: historyReducer
-
+	counterReducer: counterReducer
 });
 
 
